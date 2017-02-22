48324a1d61333af2ec4ebb7c459db2c6b6196ac3
saurabhshendye <Saurabh.shendye@gmail.com>
2017-02-21 21:04:25 -0700

    Added 2 new classes

---
+++ src/cs455/scaling/Client/client.java
@@ -0,0 +1,14 @@
+/*
+ * Created by saurabh on 2/21/2017.
+ */
+
+package cs455.scaling.Client;
+
+
+public class client {
+
+    public static void main(String [] args)
+    {
+
+    }
+}
---
+++ src/cs455/scaling/Server/Worker_Thread.java
@@ -0,0 +1,8 @@
+/*
+ * Created by saurabh on 2/21/2017.
+ */
+package cs455.scaling.Server;
+
+
+public class Worker_Thread extends Thread {
+}
