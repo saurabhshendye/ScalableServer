9445b3957bf62b7bc891aac9e43171b2ffc7bbc3
saurabhshendye <Saurabh.shendye@gmail.com>
2017-02-21 21:10:30 -0700

    Inputs read

--- src/cs455/scaling/Server/server.java
+++ src/cs455/scaling/Server/server.java
@@ -6,4 +6,12 @@ package cs455.scaling.Server;
 
 public class server {
 
+    private static int Thread_count;
+    private static int port;
+
+    public static void main(String [] args)
+    {
+        port = Integer.parseInt(args[0]);
+        Thread_count = Integer.parseInt(args[1]);
+    }
 }
