5556a5d8f973cd6ad0f3699fc6920e277dc1b2ec
saurabhshendye <Saurabh.shendye@gmail.com>
2017-02-24 00:21:32 -0700

    Created a thread package

---
+++ src/cs455/scaling/Threads/Client_send_thread.java
@@ -0,0 +1,9 @@
+/*
+ * Created by saurabh on 2/24/2017.
+ */
+
+package cs455.scaling.Threads;
+
+
+public class Client_send_thread {
+}
--- src/cs455/scaling/Threads/Worker_Thread.java
+++ src/cs455/scaling/Threads/Worker_Thread.java
@@ -1,7 +1,7 @@
 /*
  * Created by saurabh on 2/21/2017.
  */
-package cs455.scaling.Server;
+package cs455.scaling.Threads;
 
 
 public class Worker_Thread extends Thread {
