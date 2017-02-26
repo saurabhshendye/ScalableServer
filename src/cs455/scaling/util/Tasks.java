a86d837ad80ca1230e63f692b41e7d74ed841418
saurabhshendye <Saurabh.shendye@gmail.com>
2017-02-26 16:17:58 -0700

    Created constructor for Write Task

--- src/cs455/scaling/util/Tasks.java
+++ src/cs455/scaling/util/Tasks.java
@@ -4,5 +4,21 @@
 package cs455.scaling.util;
 
 
-public class Tasks {
+public class Tasks
+{
+    private int type;
+    private String hash_code;
+
+    // Write Task
+    public Tasks(int type, String hash)
+    {
+        this.hash_code = hash;
+        this.type = type;
+    }
+
+    // Read Task
+
+
+
+
 }
