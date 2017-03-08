e40c61575a3d79fe00b0511b8c843a92a34671be
saurabhshendye <Saurabh.shendye@gmail.com>
2017-03-07 19:10:27 -0700

    Server stats printer class created

--- src/cs455/scaling/Threads/ServerStatsPrinter.java
+++ src/cs455/scaling/Threads/ServerStatsPrinter.java
@@ -5,7 +5,62 @@
 package cs455.scaling.Threads;
 
 
-public class ServerStatsPrinter
-{
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.concurrent.TimeUnit;
 
+public class ServerStatsPrinter extends Thread
+{
+    private int readProcessed;
+    private int writeProcessed;
+    private final DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
+
+    public ServerStatsPrinter()
+    {
+        this.readProcessed = 0;
+        this.writeProcessed = 0;
+    }
+
+    public void run()
+    {
+        System.out.println("Stats Printer Run method");
+        while (true)
+        {
+            try {
+                TimeUnit.SECONDS.sleep(10);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+            printStats();
+            resetCounters();
+        }
+    }
+
+    public void readIncrement()
+    {
+        readProcessed++;
+    }
+
+    public void writeIncrement()
+    {
+        writeProcessed++;
+    }
+
+    private void resetCounters()
+    {
+        readProcessed = 0;
+        writeProcessed = 0;
+    }
+
+    private void printStats()
+    {
+        Date date = new Date();
+        int average = readProcessed + writeProcessed;
+        average = average/2;
+        int rate = average/5;
+        System.out.println(dateFormat.format(date) + " Current Server Throughput: "
+                            + rate + " messages/sec" + "Number of active Connections: ");
+    }
 }
