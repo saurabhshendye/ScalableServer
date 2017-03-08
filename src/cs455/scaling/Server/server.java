cfc7d63008f620464f1ae1b953c20ae1b74bf3be
saurabhshendye <Saurabh.shendye@gmail.com>
2017-03-08 13:48:50 -0700

    Added the timeout again

--- src/cs455/scaling/Server/server.java
+++ src/cs455/scaling/Server/server.java
@@ -60,7 +60,7 @@ public class server {
         // Listening for the connections
         while (true)
         {
-            int i = selector.select();
+            int i = selector.select(1);
 
 //            selector.select();
             if (i> 0)
